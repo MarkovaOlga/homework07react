ffdd3c2da48bc0a74716552c715646baf3c8603c
MarkovaOlga <olgamarkova85@ya.ru>
2024-05-12 10:23:48 +0300

    hometask07 added

---
+++ src/data/tasks.js
@@ -0,0 +1,9 @@
+const tasks = [
+    { id: 1, title: "Task 01", completed: true},
+    { id: 2, title: "Task 02", completed: true},
+    { id: 3, title: "Task 03", completed: false},
+    { id: 4, title: "Task 04", completed: false},
+    { id: 5, title: "Task 05", completed: true},
+];
+
+export default tasks;
---
+++ src/reducers/tasksSlice.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import tasks from "../data/tasks";
+
+export const fetchTasks = createAsyncThunk("tasks/fetchTasks", async () => {
+    return new Promise((resolve) =>
+        setTimeout(() => resolve({ data: tasks }), 1000)
+    );
+});
+const tasksSlice = createSlice({
+    name: "task",
+    initialState: {
+        entities: [],
+        loading: false,
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchTasks.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchTasks.fulfilled, (state, action) => {
+            state.entities = action.payload.data;
+            state.loading = false;
+        });
+    },
+});
+export default tasksSlice.reducer;
